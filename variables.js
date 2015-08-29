316db79f9dd087bfb075c6f4dc92d58871d3f882
ntammana <n_tammana@u.pacific.edu>
2015-08-29 01:39:00 -0700

    variables.js file added

---
+++ variables.js
@@ -0,0 +1,221 @@
+var dressColor;
+var dressSize;
+var pantColor;
+var pantSize;
+var skirtColor;
+var skirtSize;
+var shirtColor;
+var shirtSize;
+var background;
+
+if (dressColor = "black")
+{
+	if (dressSize = "small")
+	{
+	}
+	else if (dressSize = "medium")
+	{
+	}
+	else if (dressSize = "large")
+	{
+	}
+}
+else if (dressColor = "blue")
+{
+	if (dressSize = "small")
+	{
+	}
+	else if (dressSize = "medium")
+	{
+	}
+	else if (dressSize = "large")
+	{
+	}
+}
+else if (dressColor = "gray")
+{
+	if (dressSize = "small")
+	{
+	}
+	else if (dressSize = "medium")
+	{
+	}
+	else if (dressSize = "large")
+	{
+	}
+}
+else if (dressColor = "green")
+{
+	if (dressSize = "small")
+	{
+	}
+	else if (dressSize = "medium")
+	{
+	}
+	else if (dressSize = "large")
+	{
+	}
+}
+
+if (skirtColor = "black")
+{
+	if (skirtSize = "small")
+	{
+	}
+	else if (skirtSize = "medium")
+	{
+	}
+	else if (skirtSize = "large")
+	{
+	}
+}
+else if (skirtColor = "blue")
+{
+	if (skirtSize = "small")
+	{
+	}
+	else if (skirtSize = "medium")
+	{
+	}
+	else if (skirtSize = "large")
+	{
+	}
+}
+else if (skirtColor = "gray")
+{
+	if (skirtSize = "small")
+	{
+	}
+	else if (skirtSize = "medium")
+	{
+	}
+	else if (skirtSize = "large")
+	{
+	}
+}
+else if (skirtColor = "green")
+{
+	if (skirtSize = "small")
+	{
+	}
+	else if (skirtSize = "medium")
+	{
+	}
+	else if (skirtSize = "large")
+	{
+	}
+}
+
+if (pantColor = "black")
+{
+	if (pantSize = "small")
+	{
+	}
+	else if (pantSize = "medium")
+	{
+	}
+	else if (pantSize = "large")
+	{
+	}
+}
+else if (pantColor = "blue")
+{
+	if (pantSize = "small")
+	{
+	}
+	else if (pantSize = "medium")
+	{
+	}
+	else if (pantSize = "large")
+	{
+	}
+}
+else if (pantColor = "gray")
+{
+	if (pantSize = "small")
+	{
+	}
+	else if (pantSize = "medium")
+	{
+	}
+	else if (pantSize = "large")
+	{
+	}
+}
+else if (pantColor = "green")
+{
+	if (pantSize = "small")
+	{
+	}
+	else if (pantSize = "medium")
+	{
+	}
+	else if (pantSize = "large")
+	{
+	}
+}
+
+if (shirtColor = "black")
+{
+	if (shirtSize = "small")
+	{
+	}
+	else if (shirtSize = "medium")
+	{
+	}
+	else if (shirtSize = "large")
+	{
+	}
+}
+else if (shirtColor = "blue")
+{
+	if (shirtSize = "small")
+	{
+	}
+	else if (shirtSize = "medium")
+	{
+	}
+	else if (shirtSize = "large")
+	{
+	}
+}
+else if (shirtColor = "gray")
+{
+	if (shirtSize = "small")
+	{
+	}
+	else if (shirtSize = "medium")
+	{
+	}
+	else if (shirtSize = "large")
+	{
+	}
+}
+else if (shirtColor = "green")
+{
+	if (shirtSize = "small")
+	{
+	}
+	else if (shirtSize = "medium")
+	{
+	}
+	else if (shirtSize = "large")
+	{
+	}
+}
+
+if (background = "beach")
+{
+}
+else if (background = "school")
+{
+}
+else if (background = "mall")
+{
+}
+else if (background = "field")
+{
+}
+else if (background = "forest")
+{
+}
